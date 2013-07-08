b9098a1f238692daab46365b6ed9583a80cd8f19
bomi1012 <bomi1012@hs-karlsruhe.de>
2013-07-08 23:03:11 +0200

    css

--- includes/header_oben.php
+++ includes/header_oben.php
@@ -16,7 +16,7 @@ $admin->CoockieStatus();
         </div>
     <?php } ?>
     <a href="http://www.tagesmutter-landau.de">
-        <img src="<?php echo Constans::IMAGE_PATCH ?>tm_kinder3.png" alt="<?php echo Constans::UEBERSCHRIFT_INDEX_1 . " " . Constans::KW_TAGESMUTTER ?>" style="height: 161px; ">
+        <img src="<?php echo Constans::IMAGE_PATCH ?>tm_kinder3.png" alt="<?php echo Constans::UEBERSCHRIFT_INDEX_1 . " " . Constans::KW_TAGESMUTTER ?>" class="img-header-oben">
     </a>
 </div>
 <div class="text_oben">
