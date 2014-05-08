820ba3b6142b064cefcfae160473ff3d07f06cab
bomi1012 <bomi1012@hs-karlsruhe.de>
2014-05-08 23:23:16 +0200

    Refactoring

--- resources/text/c2_about.php
+++ resources/text/c2_about.php
@@ -2,32 +2,40 @@
     <br>
     <!-- http://fancyapps.com/fancybox/ -->
     <div id="photo_olga">
-    <a title="Ich bin <?php echo Constans::KW_NAME ?>" class="fancybox" rel="group" href="<?php echo Constans::IMAGE_PATCH ?>tm_photo_1b.jpg">
-        <img class="curved" src="<?php echo Constans::IMAGE_PATCH ?>tm_photo_1a.jpg" alt="<?php echo Constans::KW_ROTKAEPPCHEN ?>"></a>
+        <a 
+            title="Ich bin Olga Böhm" 
+            class="fancybox" 
+            rel="group" 
+            href="<?php echo Constans::IMAGE_PATCH ?>tm_photo_1b.jpg">
+            <img 
+                class="curved" 
+                src="<?php echo Constans::IMAGE_PATCH ?>tm_photo_1a.jpg" 
+                alt="<?php echo Constans::KW_ROTKAEPPCHEN ?>">
+        </a>
 
-    <a title="Ich und meine Kinder (Gabriel und Evelin)" class="fancybox" rel="group" href="<?php echo Constans::IMAGE_PATCH ?>tm_photo_2b.jpg">
-        <img class="curved" src="<?php echo Constans::IMAGE_PATCH ?>tm_photo_2a.jpg" alt="<?php echo Constans::KW_ROTKAEPPCHEN ?>"></a>
-   </div>
-    
+        <a title="Ich und meine Kinder (Gabriel und Evelin)" class="fancybox" rel="group" href="<?php echo Constans::IMAGE_PATCH ?>tm_photo_2b.jpg">
+            <img class="curved" src="<?php echo Constans::IMAGE_PATCH ?>tm_photo_2a.jpg" alt="<?php echo Constans::KW_ROTKAEPPCHEN ?>"></a>
+    </div>
+
     <div id="photo_hermann">
-         <a title="Ich und meine Kinder (Gabriel und Evelin)" class="fancybox" rel="group" href="<?php echo Constans::IMAGE_PATCH ?>tm_photo_3b.jpg">
-        <img class="curved" src="<?php echo Constans::IMAGE_PATCH ?>tm_photo_3a.jpg" alt="<?php echo Constans::KW_ROTKAEPPCHEN ?>"></a>
- 
-    <a title="Ich bin <?php echo Constans::KW_NAME ?>" class="fancybox" rel="group" href="<?php echo Constans::IMAGE_PATCH ?>tm_photo_4b.jpg">
-        <img class="curved" src="<?php echo Constans::IMAGE_PATCH ?>tm_photo_4a.jpg" alt="<?php echo Constans::KW_ROTKAEPPCHEN ?>"></a>
+        <a title="ich bin Hermann Böhm" class="fancybox" rel="group" href="<?php echo Constans::IMAGE_PATCH ?>tm_photo_3b.jpg">
+            <img class="curved" src="<?php echo Constans::IMAGE_PATCH ?>tm_photo_3a.jpg" alt="<?php echo Constans::KW_ROTKAEPPCHEN ?>"></a>
 
-     </div>
-    
-        <br />
-    <h2><?php echo $model->Umlaute("Olga Böhm")?></h2>
+        <a title="Ich und meine Kinder (Gabriel und Evelin)" class="fancybox" rel="group" href="<?php echo Constans::IMAGE_PATCH ?>tm_photo_4b.jpg">
+            <img class="curved" src="<?php echo Constans::IMAGE_PATCH ?>tm_photo_4a.jpg" alt="<?php echo Constans::KW_ROTKAEPPCHEN ?>"></a>
+
+    </div>
+
+    <br />
+    <h2><?php echo $model->Umlaute("Olga Böhm") ?></h2>
     <p>
         <img class="icon" src="<?php echo Constans::ICON_PATCH ?>check.png" alt="check" style="width: 16px;">
         <span id="about_allgemein_olga" class="navi"><?php echo $model->Umlaute("Über mich") ?></span><br>
         <img class="icon" src="<?php echo Constans::ICON_PATCH ?>check.png" alt="check" style="width: 16px;">
         <span id="about_erfahrung_olga" class="navi">Meine Erfahrung</span><br>    
     </p>
-    
-     <h5 class="margin_h">Downloads:</h5>
+
+    <h5 class="margin_h">Downloads:</h5>
     <p>
         <img class="icon" src="<?php echo Constans::ICON_PATCH ?>pdf.png" alt="check" style="width: 16px;">
         <a href="<?php echo Constans::PDF_PATCH ?>olga/abschlusszertifikat.pdf" target="_blank">Abschlusszertifikat</a><br> 
@@ -38,15 +46,15 @@
         <img class="icon" src="<?php echo Constans::ICON_PATCH ?>pdf.png" alt="check" style="width: 16px;">
         <a href="<?php echo Constans::PDF_PATCH ?>olga/erste_hilfe_kurs.pdf" target="_blank">Erste-Hilfe am Kind</a><br> 
     </p>
-    
-    <h2><?php echo $model->Umlaute("Hermann Böhm")?></h2>
+
+    <h2><?php echo $model->Umlaute("Hermann Böhm") ?></h2>
     <p>
         <img class="icon" src="<?php echo Constans::ICON_PATCH ?>check.png" alt="check" style="width: 16px;">
         <span id="about_allgemein_hermann" class="navi"><?php echo $model->Umlaute("Über mich") ?></span><br>
         <img class="icon" src="<?php echo Constans::ICON_PATCH ?>check.png" alt="check" style="width: 16px;">
         <span id="about_erfahrung_hermann" class="navi">Meine Erfahrung</span><br>  
     </p>
- <h5 class="margin_h">Downloads:</h5>
+    <h5 class="margin_h">Downloads:</h5>
     <p>
         <img class="icon" src="<?php echo Constans::ICON_PATCH ?>pdf.png" alt="check" style="width: 16px;">
         <a href="<?php echo Constans::PDF_PATCH ?>hermann/abschlusszertifikat.pdf" target="_blank">Abschlusszertifikat</a><br> 
