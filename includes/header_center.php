c23dcd6b900b4c59fbd5ed5eca228322c4b3a91c
bomi1012 <bomi1012@hs-karlsruhe.de>
2014-07-07 20:37:17 +0200

    bilder gelöscht new Rotkäppchen

--- includes/header_center.php
+++ includes/header_center.php
@@ -1,13 +1,14 @@
 <?php
-$width = "190px";
-if ($model->getPage() == Constans::PAGE_INDEX) {
+//$width = "190px";
+//if ($model->getPage() == Constans::PAGE_INDEX) {
     $width = "250px";
-}
+//}
 ?>
 <div class="block_center">
     <div class="bild_gross">
          <a href="http://www.tagesmutter-landau.de">
-        <img src="<?php echo Constans::IMAGE_PATCH ?>tm_rotes_kappchen_gross_2.png" alt="<?php echo Constans::KW_ROTKAEPPCHEN . " " . Constans::KW_TAGESMUTTER ?>" style="width: <?php echo $width; ?>">
+        <img src="<?php echo Constans::IMAGE_PATCH ?>tm_rotes_kappchen_gross_2.png" alt="<?php echo Constans::KW_ROTKAEPPCHEN . " " . Constans::KW_TAGESMUTTER ?>" 
+                style="width: <?php echo $width; ?>; padding-top: 35px; padding-bottom: 35px;">
     </a>
         </div>
     <div style="float: left;">
@@ -18,7 +19,7 @@ if ($model->getPage() == Constans::PAGE_INDEX) {
         <div style="padding-top: 25px;">                                
             <span  class="bonbon bonbon_gross red" style="line-height: 1"><?php echo Constans::KW_ROTKAEPPCHEN ?></span>
         </div>
-        <?php if ($model->getPage() == Constans::PAGE_INDEX) { ?>
+        <?php //if ($model->getPage() == Constans::PAGE_INDEX) { ?>
             <div class="text_center">
                 <blockquote>
                     <p style="font-size: 16px;">
@@ -26,6 +27,6 @@ if ($model->getPage() == Constans::PAGE_INDEX) {
                     </p>
                 </blockquote>
             </div>
-        <?php } ?>
+        <?php// } ?>
     </div>
 </div>
