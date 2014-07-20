e199e72fde6726d8e25f64ba79606fcece18a1e8
bomi1012 <bomi1012@hs-karlsruhe.de>
2014-07-20 23:03:36 +0200

    2014--> 2013

--- resources/text/c2_album.php
+++ resources/text/c2_album.php
@@ -1,10 +1,10 @@
 <div class="article_text">
-    <h2>Albums auswählen:</h2>
+    <h2><?php echo $model->Umlaute("Albums auswählen:"); ?></h2>
     
      <img class="icon" src="<?php echo Constans::ICON_PATCH ?>photo.png" alt="photo" style="width: 16px; margin-left: 50px;">
      <span id="album_2013" class="navi">
          <a href="album.php?jahr=2013">Bilder aus dem Jahr 2013</a>
      </span><br> 
      <img class="icon" src="<?php echo Constans::ICON_PATCH ?>photo.png" alt="photo" style="width: 16px; margin-left: 50px;">
-     <span id="album_2014" class="navi"><a href="album.php?jahr=2014">Bilder aus dem Jahr 2013</a></span><br> 
+     <span id="album_2014" class="navi"><a href="album.php?jahr=2014">Bilder aus dem Jahr 2014</a></span><br> 
 </div>
