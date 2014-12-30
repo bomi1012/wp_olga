3c9b7ae24963a441f198b83a83b49105365127cc
bomi1012 <bomi1012@hs-karlsruhe.de>
2014-12-30 10:13:19 +0100

    gestäbuch

--- resources/text/c1_gaestebuch.php
+++ resources/text/c1_gaestebuch.php
@@ -1,20 +1,31 @@
 <?php
+(substr(date('d'), 0, 1) == 0) 
+        ? $zahl_von_date = substr(date('d'), 1, 1) 
+        : $zahl_von_date = substr(date('d'), 0, 1);
+
 $gbService = new GastBookService();
 if (isset($_POST["submit"])) {
-    $gbModel = new GastBookModel();
-    $name = $model->Umlaute($_POST[Constans::NAME]);
-    $nachricht = $model->Umlaute($_POST[Constans::NACHRICHT]);
-    $gbModel->setFromForm($name, $_POST[Constans::EMAIL], $nachricht);
-    $gbService->InsertInDB($gbModel);
-    if (is_numeric($gbModel->getId()) && $gbModel->getId() > 0) {
-        $gbService->NachrichtSenden($gbModel)
-        ?>
-        <div class="alert alert-info alert-oben">
-            <a href="#" class="close right" data-dismiss="alert">&times;</a>
-            <strong>Vielen Dank!</strong> Ihr Kommentar wird nach dem Pr&uuml;fen ver&ouml;ffentlicht
-        </div>
-        <?php
-    }
+    if ($_POST["frage"] == $model->createFrage($zahl_von_date, 10)) {
+        $gbModel = new GastBookModel();
+        $name = $model->Umlaute($_POST[Constans::NAME]);
+        $nachricht = $model->Umlaute($_POST[Constans::NACHRICHT]);
+        $gbModel->setFromForm($name, $_POST[Constans::EMAIL], $nachricht);
+        $gbService->InsertInDB($gbModel);
+        if (is_numeric($gbModel->getId()) && $gbModel->getId() > 0) {
+            $gbService->NachrichtSenden($gbModel)
+            ?>
+                    <div class="alert alert-info alert-oben">
+                        <a href="#" class="close right" data-dismiss="alert">&times;</a>
+                        <strong>Vielen Dank!</strong> Ihr Kommentar wird nach dem Pr&uuml;fen ver&ouml;ffentlicht
+                    </div>
+            <?php
+        }
+    } else {  ?>
+            <div class="alert alert-error alert-oben">
+        <a href="#" class="close right" data-dismiss="alert">&times;</a>
+        Ihre Nachricht wurde nicht gesendet, da die Antwort falsch war.
+    </div>
+<?php    }
 }
 ?>
 <!-- Lesen -->
@@ -77,6 +88,18 @@ if (isset($_POST["submit"])) {
         <div class="leerzeile"></div>
 
         <textarea required rows="6" class="input-xlarge-area1 norezise" name="<?php echo Constans::NACHRICHT ?>" id="id_<?php echo Constans::NACHRICHT ?>"></textarea>
+        <div class="leerzeile"></div>
+        
+         <label> 
+                <acronym title="Damit wir sicher sein, dass Sie ein Mensch sind, bitte geben Sie den Code aus dem Bild.">
+                    <?php echo $zahl_von_date . ' plus I0 = *'; ?> 
+                </acronym>
+            </label>
+        
+
+        <div class="input-prepend"><span class="add-on"><i class="icon-question"></i></span>
+            <input required type="text"  id="id_frage" name="frage">
+        </div>  
         <div class="leerzeile"></div>
 
         <button type="submit" class="btn btn-success right leerzeichen" name="submit">
